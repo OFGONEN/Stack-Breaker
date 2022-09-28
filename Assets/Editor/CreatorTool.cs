1954dfed58a8b29383faedc5a420f73cd49649c0
ofgonen_office <ofgonen@gmail.com>
2022-09-28 23:15:58 +0300

    CreatorTool: Level Create

--- Assets/Editor/CreatorTool.cs
+++ Assets/Editor/CreatorTool.cs
@@ -12,6 +12,12 @@ using Sirenix.OdinInspector;
 public class CreatorTool : ScriptableObject
 {
 #region Fields
+    [ FoldoutGroup( "Level Create" ), SerializeField ] int level_stack_count;
+    [ FoldoutGroup( "Level Create" ), SerializeField ] float level_stack_buffer;
+    [ FoldoutGroup( "Level Create" ), SerializeField ] float level_cylinder_scale_cofactor;
+    [ FoldoutGroup( "Level Create" ), SerializeField ] float level_player_offset;
+    [ FoldoutGroup( "Level Create" ), SerializeField ] StackData level_stack_data;
+
     [ FoldoutGroup( "Place Stack Piece" ), SerializeField ] Transform place_stack_parent;
     [ FoldoutGroup( "Place Stack Piece" ), SerializeField ] PlaceStackData place_stack_data;
 #endregion
@@ -26,6 +32,35 @@ public class CreatorTool : ScriptableObject
 #endregion
 
 #region Implementation
+    [ Button() ]
+    void CreateLevel()
+    {
+		var stackParent = GameObject.Find( "stack_parent" ).transform;
+		var cylinder    = GameObject.Find( "gfx_cylinder" ).transform;
+		var player      = GameObject.FindWithTag( "Player" ).transform;
+
+		stackParent.DestroyAllChildren();
+
+		float verticalPosition  = 0;
+		float lastStackPosition = 0;
+
+		for( var i = 0; i <= level_stack_count; i++ )
+        {
+			var stack = PrefabUtility.InstantiatePrefab( level_stack_data.stack_gameObject ) as GameObject;
+			stack.transform.SetParent( stackParent );
+			stack.transform.localPosition = Vector3.up * verticalPosition;
+
+			lastStackPosition  = verticalPosition;
+			verticalPosition  += level_stack_buffer + level_stack_data.stack_height;
+		}
+
+		cylinder.localScale = Vector3.one.SetY( verticalPosition * level_cylinder_scale_cofactor );
+		player.position     = ( Vector3.up * lastStackPosition ).SetZ( level_player_offset );
+		player.forward = Vector3.right;
+
+		Camera.main.GetComponent< CameraFollow >().ResetPosition();
+	}
+
     [ Button() ]
     void PlaceStackPieces() 
     {
@@ -44,6 +79,13 @@ public class CreatorTool : ScriptableObject
 #if UNITY_EDITOR
 #endif
 #endregion
+}
+
+[ Serializable ]
+public struct StackData
+{
+    public GameObject stack_gameObject;
+	public float stack_height;
 }
 
 [ Serializable ]
