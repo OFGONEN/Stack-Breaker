66c50fad02e09711bcc6fb4c3cc47f1d04b00152
ofgonen_office <ofgonen@gmail.com>
2022-09-29 17:14:36 +0300

    Player: Don't change scale of the collider

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -173,7 +173,7 @@ public class Player : MonoBehaviour
 		var scale = GameSettings.Instance.player_width_range.ReturnProgress( notif_player_width.sharedValue );
 
 		transform_width.localScale     = new Vector3( scale, 1, scale );
-		_collider.transform.localScale = new Vector3( scale, 1, scale );
+		// _collider.transform.localScale = new Vector3( scale, 1, scale );
 	}
 
 	void PunchScalePlayer_OnGround()
